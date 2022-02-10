2667afcd54b71324d29f7392f83af73ecd1a0cf1
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-02-10 08:47:22 -0500

    Adding in Student class

---
+++ 01-StoreData/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+// feilds, instance varaibles (data)
+private String id;
+private String name;
+private boolean closeContact;
+private int [] grades;
+
+//constructor method
+public student(String id, Strin n, boolean cc, int[] g){
+  this.id = id;
+  this.name = n;
+  
+}
+
+//behaviors (methods)
+
+//get the close closeContact status of thr student
+public boolean isCloseContact(){
+
+}//end isCloseContact
+
+public void setCloseContact(boolean value) {
+
+}//end setCloseContact
+
+
+//see if student is failing
+public boolean isFailing(){
+
+
+}//end isFailing
+
+public String getName(){
+
+}//end getName
+
+}//end class
